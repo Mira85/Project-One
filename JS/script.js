53694bae0175dad8788009070918e1cf738f6fa1
Mira Makadia <patelmirak@gmail.com>
2021-10-27 22:48:08 -0400

    WIP: completed submit function

--- JS/script.js
+++ JS/script.js
@@ -2,13 +2,45 @@
 const BASE_URL = 'https://travelbriefing.org/';
 // State Variables
 let countryData;
+let country;
 // Cached Element References
 const $form = $('form');
+const $inputCountry = $('#inputCountry');
+const $main = $('main');
 // Event Listeners
 $form.on('submit', handleSubmit);
 // Functions
 
-function handleSubmit (evt) {
-    evt.preventDefault(); 
-    console.log('clicked');
+function handleSubmit(evt) {
+    evt.preventDefault();
+    country = $inputCountry.val();
+
+
+    $.ajax(`${BASE_URL}${country}?format=json`).then(function (data) {
+        // Getting data which is a list and converting to an JS object
+        countryData = JSON.parse(data);
+        // success callback function
+        console.log(countryData);
+        render();
+    }, function (error) {
+        // failure callback function
+        console.log(error);
+
+    })
 }
+
+function render() {
+    $main.html(`
+<h3>${countryData.names.name}</h3>
+<p id="advise"> Advise: ${countryData.advise.UA.advise}</p>
+<p id="currency"> Currency: ${countryData.currency.name}</p>
+<p id="currency">Currency Rate: ${countryData.currency.rate}</p>
+<p id="calling-code"> Calling Code: ${countryData.telephone.calling_code}</p>
+
+
+`);
+
+}
+
+/*<p id="vaccination-req">${countryData.vaccination}</p>
+<p id="weather">${countryData.Rated}</p>*/
---
+++ README.md
@@ -0,0 +1,21 @@
+# Happy Journey
+
+This simple web application provides some important information for visiting any country in the world. It gives advise for travel, currency rate, country-calling-code, vaccination requirement and weather in destination country for requested month
+
+## Technologies Used
+
+- HTML5
+- CSS3
+- Javascript/JQuery
+- Travelbriefing API
+
+
+## Screenshots
+
+## Getting started
+
+Click here to see the deployed app!
+
+## Future Enhancements
+
+
